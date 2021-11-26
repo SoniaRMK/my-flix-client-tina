aeb27f7e1e64db7ee81decc29edff85fa660accb
Mbabazi Moreen <mmbabazi2@gmail.com>
2021-11-26 06:18:52 +0300

    made changes to main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -142,7 +142,7 @@ class MainView extends React.Component {
                 </Col>
                 if (movies.length === 0) return <div className="main-view" />;
                 return <Col md={8}>
-                  <ProfileView movies={movies} movie={movies} />
+                  <ProfileView movies={movies} />
                 </Col>
               }} />
             </Row>
